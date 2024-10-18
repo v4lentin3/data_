13594dacc4929f3a28a7dbe7a9881b96684863a5
v4lentin3 <80768354+v4lentin3@users.noreply.github.com>
2024-10-18 17:03:12 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from cabecalhos import *  # Certifique-se de que este módulo existe e está corretamente configurado.
 
 # Configurações da página
 st.set_page_config(
